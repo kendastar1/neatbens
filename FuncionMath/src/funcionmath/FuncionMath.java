8f2ce09fd8ba95678174ce876c3b871a5b2b109f
kendastar1 <126899253+kendastar1@users.noreply.github.com>
2023-09-21 06:26:37 -0500

    Add files via upload

---
+++ FuncionMath/src/funcionmath/FuncionMath.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funcionmath;
+
+
+
+
+/**
+ *
+ * @author ESTUDIANTE11
+ */
+public class FuncionMath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("El Numero minimo es " + Math.min(a,b));
+        int c = 10;
+        int d = 20;
+        System.out.println("el numero maximo es " + Math.max(c, d));
+        System.out.println("Pi equivale a " + Math.PI);
+        double z = 10;
+        System.out.println("El numero es " + Math.abs(z));
+        System.out.println("El numero es " + Math.expm1(3));
+        System.out.println("El numero es " + Math.cos(d));
+        System.out.println("El numero es " + Math.multiplyExact(a,b) + " " + Math.multiplyExact(c,d));
+        System.out.println("El numero es " + Math.incrementExact(a));
+        System.out.println("El numero es " + Math.decrementExact(b));
+        System.out.println("El numero es " + Math.random()* a);
+        System.out.println("El numero es " + Math.floorMod(c,d));
+        System.out.println("El numero es " + Math.floorDiv(d, c));
+        
+        
+        
+        
+         
+
+        
+    }
+    
+}
